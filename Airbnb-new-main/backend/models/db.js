56cb136d0d9d974bcb740cebe3fafa47b5ff35f9
Samarpit088 <dev.samarpit.mains@gmail.com>
2024-12-21 16:00:50 +0530

    Delete Airbnb-new-main directory

--- Airbnb-new-main/backend/models/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-const mongo_url=process.env.MONGO_CONN;
-mongoose.connect(mongo_url).then(()=>{
-    console.log("connection successful");
-}).catch((err)=>{
-    console.log("mongo db connection error",err);
-});
